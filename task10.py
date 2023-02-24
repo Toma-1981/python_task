0449d5585e1340fa3576837bba595d7bbe2d54ff
Tamara <Tamara-1981_0@mail.ru>
2023-02-24 23:16:33 +0700

    задача 10

--- task10.py
+++ task10.py
@@ -20,6 +20,21 @@ if side_eagle < side_tails:
     print(f'нужно перевернуть {side_eagle} орла')
 else:
     print(f'нужно перевернуть {side_tails} решки')
+
+# n = int(input())
+# count_zero = 0
+# count_one = 0
+# for i in range(n):
+#     x = int(input())
+# if x == 0:
+#     count_zero += 1
+# else:
+#     count_one += 1
+# if count_one > count_zero:
+#     print(count_zero)
+# else:
+#     print(count_one)
+    
                     
 
 
